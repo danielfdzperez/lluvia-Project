a0d276da40510ace1ba0c59f5de68987650d51b8
danielfdzperez <daniel.fdz.perez@gmail.com>
2014-04-26 18:34:35 +0200

    persecucion

--- vendor/boids/javascript/main.js
+++ vendor/boids/javascript/main.js
@@ -1,3 +1,5 @@
+logger = document.getElementById("logger")
+
 function main(){
   var w = new World()
   var boid_list = new WorldInterface("boid_list_content")
@@ -6,96 +8,55 @@ function main(){
   var boid_editor = new BoidEditor("boid_properties")
   boid_list.addPort("focus_boid", boid_editor)
 
-  /* Example 1 of Boid creation */
-   var fixed_target = w.new_boid( { name: "fixed", colour: "green",
+   var caco1 = w.new_boid( { colour: "red",
                  geo_data:  {
-                               position: new Vector(220, 230),
-                               velocity: new Vector(0, 0),
+                               position: new Vector(300, 180),
+                               velocity: new Vector(60, 0),
                                acceleration: new Vector(0, 0)
                             }
    })
 
-
-
-     w.new_boid( {name: "seeker", colour: "yellow"}, function(config) {
-                           /* Here you can interact with the outer scope */
-                           /* You can also access the already created brain */
-                           config.geo_data = {
-                              position: new Vector(100, 100),
-                              velocity: new Vector(10, 10),
-                              acceleration: new Vector(0, 0)
-                           }
-                           config.brain.activate("seek", fixed_target)
-                           return config
-    })
-
-
-    /* Example: wander behavior */
-  var wanderer = []
-  for(var i = 0; i<10; i++)
-    wanderer.push( w.new_boid( function (config) {
-      config.color = "purple"
-      config.brain.activate("wander")
-      return config
-    }))
-
-   /* Example: seek behavior */
-   var seeker = []
-    var t = w.new_boid( function(config) {
-                           /* Here you can interact with the outer scope */
-                           config.colour   = "red"
-                           config.geo_data = {
-                              position: new Vector(200, 200),
-                              velocity: new Vector(0, 0),
-                              acceleration: new Vector(0, 0)
-                           }
-                           return config
-    })
-
-   var first = t
-   seeker.push(t)
-
-   for(var i=0; i<5; i++)
-     seeker.push( t = w.new_boid( function(config) {
-       config.brain.activate('seek', t)
-     }) )
-
-   first.brain.activate('seek', t)
-
-
-   /*  Example: flee behavior */
-   var fleer = []
-   for (var i=0; i<8; i++) {
-     var f
-     fleer.push( f = w.new_boid( function(config) {
-       config.color = "silver"
-       config.vel_max = 10
-       config.brain.activate("flee", wanderer[i % wanderer.length] )
-     } ))
-   }
-
-   /*  Example: pursue behaviour*/
-   var b2 = w.new_boid( function (config) {
-     config.colour  = "lime"
-     config.vel_max = 80
-     config.force_limits.thrust   = 40
-     config.force_limits.steering = 80
-     config.geo_data.position     = new Vector(0, 0)
-     config.brain.activate("pursue", first)
+var caco2 = w.new_boid( { colour: "red",
+                 geo_data:  {
+                               position: new Vector(100, 100),
+                               velocity: new Vector(0, 60),
+                               acceleration: new Vector(0, 0)
+                            }
    })
 
-   /*  Example: pursue behaviour*/
-   var b3 = w.new_boid( function (config) {
-     config.colour  = "fuchsia"
-     config.vel_max = 80
-     config.force_limits.thrust   = 40
-     config.force_limits.steering = 80
-     config.geo_data.position     = new Vector(0, 0)
-     config.brain.activate("seek", first)
-     //config.brain.activate("pursue<seek", first)
-     return config
-   })
+var policia = w.new_boid({
+         colour: "blue",
+	})
+         policia.vel_max = 100
+	 policia.brain.activate('seek')
+	 policia.brain.get_behavior('seek').set_target(caco1)
+
+	 function persecucion(){
+	     var policia_caco1 = (caco1.geo_data.position.get_coord(0) - policia.geo_data.position.get_coord(0))
+	     var policia_caco2 = (caco2.geo_data.position.get_coord(1) - policia.geo_data.position.get_coord(1))
+		 if(policia_caco1 < 0)
+		     policia_caco1 *= -1
+	         if(policia_caco2 < 0)
+		     policia_caco2 *= -1
+
+	     if( caco1.geo_data.position.get_coord(0) >= 400)
+		 caco1.geo_data.velocity = new Vector(-60,0);
+
+		     if(caco1.geo_data.position.get_coord(0) <= 10)
+			 caco1.geo_data.velocity = new Vector(60,0);
+
+		     if( caco2.geo_data.position.get_coord(1) >= 400)
+			 caco2.geo_data.velocity = new Vector(0,-60)
+                     if( caco2.geo_data.position.get_coord(1) <= 10)
+			 caco2.geo_data.velocity = new Vector(0,60)
 
 
-   w.start()
+			     if(policia_caco1 < policia_caco2)
+				 policia.brain.get_behavior('seek').set_target(caco1)
+			     else
+				 policia.brain.get_behavior('seek').set_target(caco2)
+	 }
+	 setInterval(persecucion,30);
+
+	 w.start()
 }
